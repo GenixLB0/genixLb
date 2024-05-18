191dbb84f52c0816cff017f0265c47f24b76d79b
ahmeddsaid <ahmedsaidadnan@outlook.com>
2024-05-18 13:42:05 +0300

    added projects, edited hero section

--- frontend/src/Components/HeroComponent/HeroComponent.js
+++ frontend/src/Components/HeroComponent/HeroComponent.js
@@ -4,6 +4,11 @@ import "./HeroComponent.css";
 import Hero from "../../Images/Hero3.png";
 import SubTitle from "../SubTitle/SubTitle";
 import { useNavigate } from "react-router-dom";
+import video from './../../Images/hero-video.mp4'
+import hero_img from './../../Images/whyChooseUs/vert-marq-ext1-5.jpg'
+import hero_img1 from './../../Images/whyChooseUs/vert-marq-ext1-6.jpg'
+import hero_img2 from './../../Images/whyChooseUs/vert-marq-ext1-3.jpg'
+import hero_img3 from './../../Images/whyChooseUs/vert-marq-ext1-2.jpg'
 
 export default function HeroComponent() {
   const navigate = useNavigate();
@@ -45,7 +50,7 @@ export default function HeroComponent() {
         muted
         autoPlay
         loop
-        src="assets/hero-video.mp4"
+        src={video}
       ></video>
       <div className="video-overlay"></div>
       <div className="content-hero">
@@ -67,20 +72,20 @@ export default function HeroComponent() {
           <div className="images-holder">
             <div className="left">
               <div className="images">
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
+                <img src={hero_img} alt="" />
+                <img src={hero_img1} alt="" />
+                <img src={hero_img2} alt="" />
+                <img src={hero_img3} alt="" />
+                <img src={hero_img} alt="" />
               </div>
             </div>
             <div className="right">
               <div className="images">
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
-                <img src="assets/our-project.png" alt="" />
+                <img src={hero_img} alt="" />
+                <img src={hero_img1} alt="" />
+                <img src={hero_img2} alt="" />
+                <img src={hero_img3} alt="" />
+                <img src={hero_img} alt="" />
               </div>
             </div>
           </div>
