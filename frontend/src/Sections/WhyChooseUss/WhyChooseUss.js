e9f3c93de0a4b245ff51f102d01f87172439ed3c
ahmeddsaid <ahmedsaidadnan@outlook.com>
2024-06-28 10:43:24 +0300

    build

--- frontend/src/Sections/WhyChooseUss/WhyChooseUss.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import WhyChooseUs from '../../Components/WhyChooseUs/WhyChooseUs'
-
-export default function WhyChooseUss() {
-  return (
-    <div>
-        <WhyChooseUs/>
-    </div>
-  )}
